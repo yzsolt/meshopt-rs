f5d588f9f7ce8d0023f20144c766d4d0143d7bb4
Zsolt Bölöny <bolony.zsolt@gmail.com>
2025-11-11 22:19:12 +0100

    Improve quantization utilities

--- src/quantize.rs
+++ src/quantize.rs
@@ -28,7 +28,7 @@ pub fn quantize_snorm(mut v: f32, n: u32) -> i32 {
     (v * scale + round) as i32
 }
 
-/// Quantizes a float into half-precision floating point value.
+/// Quantizes a float into half-precision (as defined by IEEE-754 fp16) floating point value.
 ///
 /// Generates +-inf for overflow, preserves NaN, flushes denormals to zero, rounds to nearest.
 ///
@@ -56,7 +56,7 @@ pub fn quantize_half(v: f32) -> u16 {
     (s | h) as u16
 }
 
-/// Quantizes a float into a floating point value with a limited number of significant mantissa bits.
+/// Quantizes a float into a floating point value with a limited number of significant mantissa bits, preserving the IEEE-754 fp32 binary representation
 ///
 /// Generates +-inf for overflow, preserves NaN, flushes denormals to zero, rounds to nearest.
 ///
@@ -77,4 +77,122 @@ pub fn quantize_float(v: f32, n: i32) -> f32 {
     ui = if e == 0 { 0 } else { ui };
 
     f32::from_bits(ui)
+}
+
+/// Reverse quantization of a half-precision (as defined by IEEE-754 fp16) floating point value
+///
+/// Preserves Inf/NaN, flushes denormals to zero
+pub fn dequantize_half(h: u16) -> f32 {
+    let s = ((h & 0x8000) as u32) << 16;
+    let em = (h & 0x7fff) as u32;
+
+    // bias exponent and pad mantissa with 0; 112 is relative exponent bias (127-15)
+    let mut r = ((em + (112 << 10)) << 13) as u32;
+
+    // denormal: flush to zero
+    r = if em < (1 << 10) { 0 } else { r };
+
+    // infinity/NaN; note that we preserve NaN payload as a byproduct of unifying inf/nan cases
+    // 112 is an exponent bias fixup; since we already applied it once, applying it twice converts 31 to 255
+    r += if em >= (31 << 10) { 112 << 23 } else { 0 };
+
+    let ui = s | r;
+
+    f32::from_bits(ui)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_quantize_float() {
+        assert_eq!(quantize_float(1.2345, 23), 1.2345);
+
+        assert_eq!(quantize_float(1.2345, 16), 1.2344971);
+        assert_eq!(quantize_float(1.2345, 8), 1.2343750);
+        assert_eq!(quantize_float(1.2345, 4), 1.25);
+        assert_eq!(quantize_float(1.2345, 1), 1.0);
+
+        assert_eq!(quantize_float(1.0, 0), 1.0);
+
+        assert_eq!(quantize_float(1.0 / 0.0, 0), 1.0 / 0.0);
+        assert_eq!(quantize_float(-1.0 / 0.0, 0), -1.0 / 0.0);
+
+        let nanf = quantize_float(0.0 / 0.0, 8);
+        assert!(nanf.is_nan());
+    }
+
+    #[test]
+    fn test_quantize_half() {
+        // normal
+        assert_eq!(quantize_half(1.2345), 0x3cf0);
+
+        // overflow
+        assert_eq!(quantize_half(65535.0), 0x7c00);
+        assert_eq!(quantize_half(-65535.0), 0xfc00);
+
+        // large
+        assert_eq!(quantize_half(65000.0), 0x7bef);
+        assert_eq!(quantize_half(-65000.0), 0xfbef);
+
+        // small
+        assert_eq!(quantize_half(0.125), 0x3000);
+        assert_eq!(quantize_half(-0.125), 0xb000);
+
+        // very small
+        assert_eq!(quantize_half(1e-4), 0x068e);
+        assert_eq!(quantize_half(-1e-4), 0x868e);
+
+        // underflow
+        assert_eq!(quantize_half(1e-5), 0x0000);
+        assert_eq!(quantize_half(-1e-5), 0x8000);
+
+        // exponent underflow
+        assert_eq!(quantize_half(1e-20), 0x0000);
+        assert_eq!(quantize_half(-1e-20), 0x8000);
+
+        // exponent overflow
+        assert_eq!(quantize_half(1e20), 0x7c00);
+        assert_eq!(quantize_half(-1e20), 0xfc00);
+
+        // inf
+        assert_eq!(quantize_half(1.0 / 0.0), 0x7c00);
+        assert_eq!(quantize_half(-1.0 / 0.0), 0xfc00);
+
+        // nan
+        let nanh = quantize_half(0.0 / 0.0);
+        assert!(nanh == 0x7e00 || nanh == 0xfe00);
+    }
+
+    #[test]
+    fn test_dequantize_half() {
+        // normal
+        assert_eq!(dequantize_half(0x3cf0), 1.234375);
+
+        // large
+        assert_eq!(dequantize_half(0x7bef), 64992.0);
+        assert_eq!(dequantize_half(0xfbef), -64992.0);
+
+        // small
+        assert_eq!(dequantize_half(0x3000), 0.125);
+        assert_eq!(dequantize_half(0xb000), -0.125);
+
+        // very small
+        assert_eq!(dequantize_half(0x068e), 1.00016594e-4);
+        assert_eq!(dequantize_half(0x868e), -1.00016594e-4);
+
+        // denormal
+        assert_eq!(dequantize_half(0x00ff), 0.0);
+        assert_eq!(dequantize_half(0x80ff), 0.0); // actually this is -0.0
+        assert_eq!(1.0 / dequantize_half(0x80ff), -1.0 / 0.0);
+
+        // inf
+        assert_eq!(dequantize_half(0x7c00), 1.0 / 0.0);
+        assert_eq!(dequantize_half(0xfc00), -1.0 / 0.0);
+
+        // nan
+        let nanf = dequantize_half(0x7e00);
+        assert!(nanf.is_nan());
+    }
 }
