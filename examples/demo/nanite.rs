ec11c8900c1a5db41d5a5206a724ce36bc1a2c1d
Zsolt Bölöny <bolony.zsolt@gmail.com>
2025-11-28 12:41:05 +0100

    demo: Miscellaneous improvements to clustered simplification

--- examples/demo/nanite.rs
+++ examples/demo/nanite.rs
@@ -5,8 +5,10 @@
 // Brian Karis. Nanite: A Deep Dive. 2021
 
 use meshopt_rs::{
+    Stream,
     cluster::{Meshlet, build_meshlets, build_meshlets_bound, compute_cluster_bounds, optimize_meshlet},
-    simplify::{SimplificationOptions, simplify},
+    index::generator::generate_vertex_remap_multi,
+    simplify::{SimplificationOptions, simplify, simplify_with_attributes},
     vertex::Vertex,
 };
 
@@ -26,6 +28,8 @@ struct Cluster {
 }
 
 const CLUSTER_SIZE: usize = 128;
+const GROUP_SIZE: usize = 8;
+const USE_LOCKS: bool = true;
 
 fn bounds(vertices: &[impl Vertex], indices: &[u32], error: f32) -> LodBounds {
     let bounds = compute_cluster_bounds(indices, vertices);
@@ -132,14 +136,14 @@ fn clusterize(vertices: &[impl Vertex], indices: &[u32]) -> Vec<Cluster> {
     clusters
 }
 
-fn partition(clusters: &[Cluster], pending: &[i32]) -> Vec<Vec<i32>> {
+fn partition(clusters: &[Cluster], pending: &[i32], _remap: &[u32]) -> Vec<Vec<i32>> {
     let mut result = Vec::new();
 
     let mut last_indices = 0;
 
     // rough merge; while clusters are approximately spatially ordered, this should use a proper partitioning algorithm
     for p in pending {
-        if result.is_empty() || last_indices + clusters[*p as usize].indices.len() > CLUSTER_SIZE * 4 * 3 {
+        if result.is_empty() || last_indices + clusters[*p as usize].indices.len() > CLUSTER_SIZE * GROUP_SIZE * 3 {
             result.push(Vec::new());
             last_indices = 0;
         }
@@ -151,16 +155,73 @@ fn partition(clusters: &[Cluster], pending: &[i32]) -> Vec<Vec<i32>> {
     result
 }
 
-fn simplify2(vertices: &[impl Vertex], indices: &[u32], target_count: usize, error: Option<&mut f32>) -> Vec<u32> {
+fn lock_boundary(locks: &mut [u8], groups: &Vec<Vec<i32>>, clusters: &[Cluster], remap: &[u32]) {
+    let mut groupmap = vec![-1i32; locks.len()];
+
+    for i in 0..groups.len() {
+        for j in 0..groups[i].len() {
+            let cluster = &clusters[groups[i][j] as usize];
+
+            for k in 0..cluster.indices.len() {
+                let v = cluster.indices[k];
+                let r = remap[v as usize] as usize;
+
+                let gr = &mut groupmap[r];
+                if *gr == -1 || *gr == i as i32 {
+                    *gr = i as i32;
+                } else {
+                    *gr = -2;
+                }
+            }
+        }
+    }
+
+    // note: we need to consistently lock all vertices with the same position to avoid holes
+    for i in 0..locks.len() {
+        let r = remap[i];
+
+        locks[i] = (groupmap[r as usize] == -2) as u8;
+    }
+}
+
+fn simplify2(
+    vertices: &[impl Vertex],
+    indices: &[u32],
+    locks: Option<&[u8]>,
+    target_count: usize,
+    error: Option<&mut f32>,
+) -> Vec<u32> {
     if target_count > indices.len() {
         return indices.to_vec();
     }
 
     let mut lod = vec![0u32; indices.len()];
-    let options = SimplificationOptions::SimplifyLockBorder
-        | SimplificationOptions::SimplifySparse
-        | SimplificationOptions::SimplifyErrorAbsolute;
-    let size = simplify(&mut lod, indices, vertices, target_count, f32::MAX, options, error);
+    let options = SimplificationOptions::SimplifySparse | SimplificationOptions::SimplifyErrorAbsolute;
+    let size = if let Some(locks) = locks {
+        let locks = locks.iter().map(|l| *l != 0).collect::<Vec<_>>();
+
+        simplify_with_attributes(
+            &mut lod,
+            indices,
+            vertices,
+            &[],
+            Some(&locks),
+            target_count,
+            f32::MAX,
+            options,
+            error,
+        )
+    } else {
+        simplify(
+            &mut lod,
+            indices,
+            vertices,
+            target_count,
+            f32::MAX,
+            options | SimplificationOptions::SimplifyLockBorder,
+            error,
+        )
+    };
     lod.truncate(size);
 
     lod
@@ -178,10 +239,17 @@ pub fn nanite(vertices: &[impl Vertex], indices: &[u32]) {
     let mut pending: Vec<_> = (0..clusters.len() as i32).collect();
 
     let mut depth = 0;
+    let mut locks = vec![0u8; vertices.len()];
+
+    // for cluster connectivity, we need a position-only remap that maps vertices with the same position to the same index
+    // it's more efficient to build it once; unfortunately, meshopt_generateVertexRemap doesn't support stride so we need to use *Multi version
+    let mut remap = vec![0u32; vertices.len()];
+    let position = Stream::from_slice(&vertices);
+    generate_vertex_remap_multi(&mut remap, Some(&indices), &[position]);
 
     // merge and simplify clusters until we can't merge anymore
     while pending.len() > 1 {
-        let groups = partition(&clusters, &pending);
+        let groups = partition(&clusters, &pending, &remap);
         pending.clear();
 
         let mut retry = Vec::new();
@@ -191,6 +259,10 @@ pub fn nanite(vertices: &[impl Vertex], indices: &[u32]) {
         let mut single_clusters = 0;
         let mut stuck_clusters = 0;
         let mut full_clusters = 0;
+
+        if USE_LOCKS {
+            lock_boundary(&mut locks, &groups, &clusters, &remap);
+        }
 
         // every group needs to be simplified now
         for i in 0..groups.len() {
@@ -217,9 +289,18 @@ pub fn nanite(vertices: &[impl Vertex], indices: &[u32]) {
                 merged.extend_from_slice(clusters[groups[i][j] as usize].indices.as_slice());
             }
 
+            let target_size = ((groups[i].len() + 1) / 2) * CLUSTER_SIZE * 3;
             let mut error = 0.0;
-            let simplified = simplify2(vertices, &merged, CLUSTER_SIZE * 2 * 3, Some(&mut error));
-            if simplified.len() as f64 > merged.len() as f64 * 0.85 || simplified.len() > CLUSTER_SIZE * 3 * 3 {
+            let simplified = simplify2(
+                vertices,
+                &merged,
+                if USE_LOCKS { Some(&locks) } else { None },
+                target_size,
+                Some(&mut error),
+            );
+            if simplified.len() as f64 > merged.len() as f64 * 0.85
+                || simplified.len() / (CLUSTER_SIZE * 3) >= merged.len() / (CLUSTER_SIZE * 3)
+            {
                 #[cfg(feature = "trace")]
                 println!(
                     "stuck cluster: simplified {} => {} over threshold",
@@ -284,13 +365,16 @@ pub fn nanite(vertices: &[impl Vertex], indices: &[u32]) {
         pending.extend_from_slice(&retry);
     }
 
+    let mut total_triangles = 0;
     let mut lowest_triangles = 0;
     for c in &clusters {
+        total_triangles += c.indices.len() / 3;
         if c.parent.error == f32::MAX {
             lowest_triangles += c.indices.len() / 3;
         }
     }
 
+    println!("total: {total_triangles} triangles in {} clusters", clusters.len());
     println!("lowest lod: {} triangles", lowest_triangles);
 
     // for testing purposes, we can compute a DAG cut from a given viewpoint and dump it as an OBJ
